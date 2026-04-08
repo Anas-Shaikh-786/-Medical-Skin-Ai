8016b185a613373af4314de8376ca9bde4dc4ca3
Anas-Shaikh-786 <anasshaikh78654@gmail.com>
2026-04-08 23:25:05 +0530

    fix CORS for production

--- api/main.py
+++ api/main.py
@@ -25,18 +25,24 @@ app = FastAPI(
     description="AI-assisted skin lesion classification (research use only)",
     version="1.0.0"
 )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:5173",
+#         "http://127.0.0.1:5173",
+#         "https://medical-skin-ai.vercel.app"
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://medical-skin-ai.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 os.makedirs("temp", exist_ok=True)
 
